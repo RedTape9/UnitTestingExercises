d2a1b550ade76552ad34d466d6b9a13970dc7e44
Sergej Jaudszims <redtape9688@gmail.com>
2023-11-04 21:42:03 +0100

    add test IsogramCheckerV2

--- src/test/java/org/example/IsogramCheckerTest.java
+++ src/test/java/org/example/IsogramCheckerTest.java
@@ -5,15 +5,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class IsogramCheckerTest {
-        @Test
-        void TestIsogramChecker() {
-            assertEquals(true, IsogramChecker.isIsogram("Dermatoglyphics"));
-            assertEquals(true, IsogramChecker.isIsogram("isogram"));
-            assertEquals(false, IsogramChecker.isIsogram("moose"));
-            assertEquals(false, IsogramChecker.isIsogram("isIsogram"));
-            assertEquals(false, IsogramChecker.isIsogram("aba"));
-            assertEquals(false, IsogramChecker.isIsogram("moOse"));
-            assertEquals(true, IsogramChecker.isIsogram("thumbscrewjapingly"));
-            assertEquals(true, IsogramChecker.isIsogram(""));
-        }
+    @Test
+    void TestIsogramChecker() {
+        assertEquals(true, IsogramChecker.isIsogram("Dermatoglyphics"));
+        assertEquals(true, IsogramChecker.isIsogram("isogram"));
+        assertEquals(false, IsogramChecker.isIsogram("moose"));
+        assertEquals(false, IsogramChecker.isIsogram("isIsogram"));
+        assertEquals(false, IsogramChecker.isIsogram("aba"));
+        assertEquals(false, IsogramChecker.isIsogram("moOse"));
+        assertEquals(true, IsogramChecker.isIsogram("thumbscrewjapingly"));
+        assertEquals(true, IsogramChecker.isIsogram(""));
     }
+
+    @Test
+    void TestIsogramChecker2() {
+        assertEquals(true, IsogramCheckerV2.isIsogram("Dermatoglyphics"));
+        assertEquals(true, IsogramCheckerV2.isIsogram("isogram"));
+        assertEquals(false, IsogramCheckerV2.isIsogram("moose"));
+        assertEquals(false, IsogramCheckerV2.isIsogram("isIsogram"));
+        assertEquals(false, IsogramCheckerV2.isIsogram("aba"));
+        assertEquals(false, IsogramCheckerV2.isIsogram("moOse"));
+        assertEquals(true, IsogramCheckerV2.isIsogram("thumbscrewjapingly"));
+        assertEquals(true, IsogramCheckerV2.isIsogram(""));
+    }
+}
